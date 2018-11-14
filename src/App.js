dcc6b479f12de8a379f8b105697dd4e6d5f7ca34
cesarsld <jaimes.cesar88@gmail.com>
2018-11-14 00:13:57 +0000

    Added pods graph

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 
-let data = {
+let dauDataset = {
     labels: [],
     datasets: [
         {
@@ -31,34 +31,97 @@ let data = {
     height: 200,
     width: 400,
 };
+
+let podDataset = {
+    labels: [],
+    datasets: [
+        {
+            label: 'Pods sold per day',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: []
+        }
+    ],
+    height: 200,
+    width: 400,
+};
+
 const options = {
     fullWidth: false,
     maintainAspectRatio: false
 };
-function fetchData() {
+function fetchDauData() {
     fetch('https://cors-anywhere.herokuapp.com/' + 'https://hidden-island-35169.herokuapp.com/api/dailyDau')
         .then(res => res.json())
         .then(dauData => {
             dauData.forEach(element => {
                 var date = new Date(element._id * 1000);
-                data.labels.push(date.toDateString())
+                dauDataset.labels.push(date.toDateString())
 
-                data.datasets[0].data.push(element.Count);
+                dauDataset.datasets[0].data.push(element.Count);
             });
-            ReactDOM.render(<Graph />, document.getElementById('root'));
+            ReactDOM.render(<Graph graphData={dauDataset} />, document.getElementById('root'));
+        });
+    fetch('https://cors-anywhere.herokuapp.com/' + 'https://hidden-island-35169.herokuapp.com/api/dailyPods')
+        .then(res => res.json())
+        .then(podData => {
+            podData.forEach(element => {
+                var date = new Date(element._id * 1000);
+                podDataset.labels.push(date.toDateString())
+
+                podDataset.datasets[0].data.push(element.Count);
+            });
+            ReactDOM.render(<Graph graphData={podDataset} />, document.getElementById('root'));
+        });
+    //ReactDOM.render(<Graph />, document.getElementById('root'));
+}
+
+function fetchPodData() {
+    fetch('https://cors-anywhere.herokuapp.com/' + 'https://hidden-island-35169.herokuapp.com/api/dailyPods')
+        .then(res => res.json())
+        .then(podData => {
+            podData.forEach(element => {
+                var date = new Date(element._id * 1000);
+                podDataset.labels.push(date.toDateString())
+
+                podDataset.datasets[0].data.push(element.Count);
+            });
+            ReactDOM.render(<Graph graphData={podDataset} />, document.getElementById('root'));
         });
 }
 
 
 class Graph extends Component {
-
-
-
     render() {
         return (
             <div>
                 <Line className="lineGraph"
-                    data={data}
+                    data={dauDataset}
+                    width={900}
+                    height={400}
+                    options={{
+                        fullWidth: false,
+                        maintainAspectRatio: false,
+                        responsive: false
+                    }}
+                />
+                <Line className="lineGraph"
+                    data={podDataset}
                     width={900}
                     height={400}
                     options={{
@@ -72,4 +135,4 @@ class Graph extends Component {
     }
 }
 
-export { Graph, fetchData };
+export { Graph, fetchDauData, fetchPodData };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Graph, fetchData } from './App';
+import { Graph, fetchDauData, fetchPodData } from './App';
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<Graph />, document.getElementById('root'));
-fetchData();
+fetchDauData();
+//fetchPodData();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
